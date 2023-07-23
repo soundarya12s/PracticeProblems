1cf2dec8ffaab47e32c120aa51c97ff03c2a91b0
S Soundarya <soundarya.s@tvsnext.com>
2023-07-23 11:45:28 +0530

    [Soundarya]Add:Added NoOfDuplicates.cs

---
+++ PracticeProblems/NumberOfDuplicates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class NumberOfDuplicates
+    {
+        public void NoOfDuplicates()
+        {
+            Console.WriteLine("Enter the size of array:");
+            int size= Convert.ToInt32(Console.ReadLine());
+            int[] array= new int[size];
+            Console.WriteLine("Enter the array elements:");
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());  
+            }
+
+            var dict = new Dictionary<int, int>();
+
+            foreach(var item in array)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict[item] = 1;
+                }
+            }
+            int count = 0;
+            foreach(var data in dict)
+            {
+                if (data.Value > 1)
+                    count += data.Value;
+            }
+            Console.WriteLine("No. Of Duplicate elements:"+count);
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -5,7 +5,27 @@ namespace PracticeProblems
     {
         static void Main(string[] args)
         {
+            bool flag= true;
             Console.WriteLine("Practice Problems:");
+            Console.WriteLine("Choose your option:\n 1.Number of Duplicates\n 2.Exit");
+            while(flag)
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        NumberOfDuplicates duplicates = new NumberOfDuplicates();
+                        duplicates.NoOfDuplicates();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+           
         }
     }
 }
