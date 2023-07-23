51bec914d43973cf35313ba6691da699ead91f94
S Soundarya <soundarya.s@tvsnext.com>
2023-07-23 12:59:11 +0530

    [Soundarya]Add:Added Pattern1.cs

--- PracticeProblems/Pattern1.cs
+++ PracticeProblems/Pattern1.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace PracticeProblems
 {
-    internal class Pattern1
+    public class Pattern1
     {
+        public void PrintGivenPattern2()
+        {
+            int rows = 7;
+            int columns = 7;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
