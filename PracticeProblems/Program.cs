8559c2484ef924709e36416c1b3dbf9122268f2b
S Soundarya <soundarya.s@tvsnext.com>
2023-07-23 10:56:32 +0530

    [Soundarya]Add:Initial commit.

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Practice Problems:");
+        }
+    }
+}
